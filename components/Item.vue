fadddb696bfc1793478fe1b62dd3fedac849a98f
sa-bt <bakhshian2020@gmail.com>
2021-09-29 21:04:18 +0330

    insert first component and set components:true in nuxt.config

---
+++ components/Item.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img :src="photo.thumbnailUrl" alt="" style="width: 300px">
+        <br>
+        {{photo.title}}
+        <br>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Item",
+        props: {
+            photo: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-for="photo in photos" :key="photo.id">
+            <Item :photo="photo" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import photos from '~/data/images'
+
+    export default {
+        name: "index",
+        data() {
+            return {
+                photos: photos
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
