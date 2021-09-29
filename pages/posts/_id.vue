0c41777cbeaf8db87af9c612abd9842750116776
sa-bt <bakhshian2020@gmail.com>
2021-09-29 23:16:08 +0330

    insert dynamic page and static file into project

---
+++ pages/posts/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img src="/images/icons/title.svg"  width="20" height="20" style="vertical-align: middle; margin-right:5px" alt="">Title: {{post.title}}
+        <br>
+        <img src="/images/icons/body.svg"  width="20" height="20" style="vertical-align: middle; margin-right:5px" alt="">Body: {{post.body}}
+    </div>
+</template>
+
+<script>
+    import posts from "~/data/posts"
+
+    export default {
+        name: "_id",
+        data() {
+            return {
+                post: {}
+            }
+        }, created() {
+            const post = posts.find((post) => post.id == this.$route.params.id)
+            this.post = post
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
