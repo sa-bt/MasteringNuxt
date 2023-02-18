384b0ac30b95eda8e743c9744e2a6df7a75be478
Pourya <bakhshian2020@gmail.com>
2023-02-19 00:54:52 +0330

    start top restaurant project

---
+++ 2-Top-Restaurants/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container alert alert-primary">
+Hello 
+
+<div class="btn btn-success">Success</div>
+    <div class="btn btn-danger">Danger</div>
+    <div class="btn btn-warning">Warning</div></div>
+</template>
+
