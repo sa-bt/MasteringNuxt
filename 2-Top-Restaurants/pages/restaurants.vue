e3f598d7e0ebd31b8d4d233c6e64a12c62535f0c
Pourya <bakhshian2020@gmail.com>
2023-02-21 20:48:59 +0330

    use pages structure in project

---
+++ 2-Top-Restaurants/pages/restaurants.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+  <Navbar/>
+  <div class="container">
+    <h1>Welcome</h1>
+  </div>
+    </div>
+  </template>
+  <style scoped>
+  .container{
+    text-align: center;
+    margin-top: 5rem;
+  }
+  </style>
+  
+  
