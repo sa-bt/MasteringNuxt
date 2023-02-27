3a1b92ba2f14506f5a74a845bd6cca8499c51f20
Pourya <bakhshian2020@gmail.com>
2023-02-27 22:12:40 +0330

    use meta in project

--- 2-Top-Restaurants/pages/restaurants/index.vue
+++ 2-Top-Restaurants/pages/restaurants/index.vue
@@ -1,16 +1,27 @@
 <template>
-  <NuxtLayout name="custom">
-    <div>
+  <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+      <Head>
+        <Title>Top 50 Restaurant in the World</Title>
+        <Meta name="description" content="My page is cool"></Meta>
+        <Link
+          rel="preload"
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        /> </Head
+    >
+  </Html>
+
+    <NuxtLayout name="custom">
       <div class="container">
         <RestaurantTable />
       </div>
-    </div>
     </NuxtLayout>
-  </template>
-  <style scoped>
-  .container {
-    text-align: center;
-    margin-top: 5rem;
-  }
-  </style>
-  
+  </div>
+</template>
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
