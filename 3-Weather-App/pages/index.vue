6cde555decdbf77c8371ec52d00e0330cf67508f
Pourya <bakhshian2020@gmail.com>
2023-03-09 22:20:39 +0330

    create front of home page app

--- 3-Weather-App/pages/index.vue
+++ 3-Weather-App/pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <NuxtWelcome/>
+  <div class="h-screen relative overflow-hidden">
+    <img src="" alt="" />
+    <div class="absolute w-full h-full top-0 overlay bg-orange-500">
+      <div class="absolute w-full h-full top-0 p-48">
+        <div class="flex justify-between">
+          <div>
+            <h1 class="text-7xl text-white">Torento</h1>
+            <p class="font-extralight text-2xl mt-2 text-white">Decomber 13</p>
+            <img src="" alt="" class="w-56 icon" />
+          </div>
+          <div>
+            <p class="text-9xl text-white font-extralight">58"</p>
+          </div>
+        </div>
+        <div class="mt-20">
+          <input type="text" class="w-1/2  h-10 " placeholder="Find a city">
+          <button class="bg-sky-400 w-20 text-white h-10 ">Search</button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
